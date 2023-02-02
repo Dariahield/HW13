624a8cec0c5f84e972670b1c9f8bf600923303a0
MONSTER B <dvnv36@mail.ru>
2023-02-02 14:48:33 +0300

    HW13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
+    /*
+    Задание
+    Реализуйте методы toString, equals и hashCode в классах Author и Book, которые вы создали на прошлом уроке.
+
+    Обратите внимание, что toString книги не должен дублировать код из toString автора, а должен делегировать (вызывать) его версию метода.
+
+
+     */
+    public static void start() {
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book = new Book(" Преступление и наказание", 1866);
+        Author author = new Author( "Федор ", "Достоевский");
+        Author author2 = new Author( "Федор ", "Достоевский");
+
+
+        System.out.println("book.bookName = " + book.getBookName());
+        System.out.println("book.bookAge = " + book.getBookAge());
+        book.setBookAge(1865);
+        System.out.println("book.setBookAge = " + book.getBookAge());
+        System.out.println("author.nameAuthor = " + author.getNameAuthor());
+        System.out.println("author.surnameAuthor = " + author.getSurnameAuthor());
+
+        Book book1 = new Book( " Война и мир", 1867);
+        Author author1 = new Author (" Лев", "Толстой");
+
+        System.out.println("book1.bookName = " + book1.getBookName());
+        System.out.println("book1.bookAge = " + book1.getBookAge());
+        System.out.println("author1.nameAuthor = " + author1.getNameAuthor());
+        System.out.println("author1.surnameAuthor = " + author1.getSurnameAuthor());
+        System.out.println(author);
+        System.out.println(author1);
+        System.out.println(book);
+        System.out.println(book1);
+
+        System.out.println("author " + author.equals(author));
+        System.out.println("author1 " + author.equals(author1));
+        System.out.println("author2 " + author.equals(author2));
+        System.out.println("Hashcode author " + author.hashCode());
+        System.out.println("book " + book.equals(book));
+        System.out.println("book1 " + book.equals(book1));
+        System.out.println("Hashcode book" + book.hashCode());
+
+
     }
 }
